c03a0ea1f9d8febb0745335c7d49e2885e93842f
honorsuper <honorsuper@126.com>
2021-06-30 16:36:24 +0800

    933.最近的请求次数

---
+++ 933.最近的请求次数.js
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=933 lang=javascript
+ *
+ * [933] 最近的请求次数
+ */
+
+// @lc code=start
+
+var RecentCounter = function () {
+  this.queue = [];
+};
+
+/**
+ * @param {number} t
+ * @return {number}
+ */
+RecentCounter.prototype.ping = function (t) {
+  // 进入队列
+  this.queue.push(t);
+  while (t - 3000 > this.queue[0]) {
+    // 说明不在范围内
+    this.queue.shift();
+  }
+  return this.queue.length;
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * var obj = new RecentCounter()
+ * var param_1 = obj.ping(t)
+ */
+// @lc code=end
